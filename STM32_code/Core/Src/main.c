5c700b869d99c2d7c18d8abbc687db8d9976c1dd
LITTLEMADNEZZ\itzic <itzicehandsome@gmail.com>
2024-09-15 21:58:29 +0700

    Init for Ex6

--- STM32_code/Core/Src/main.c
+++ STM32_code/Core/Src/main.c
@@ -93,29 +93,29 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, 0);
+	  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, 1);
 	  HAL_Delay(1000);
-	  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, 0);
+	  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, 1);
 	  HAL_Delay(1000);
-	  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, 0);
+	  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, 1);
 	  HAL_Delay(1000);
-	  HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, 0);
+	  HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, 1);
 	  HAL_Delay(1000);
-	  HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, 0);
+	  HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, 1);
 	  HAL_Delay(1000);
-	  HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, 0);
+	  HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, 1);
 	  HAL_Delay(1000);
-	  HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, 0);
+	  HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, 1);
 	  HAL_Delay(1000);
-	  HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, 0);
+	  HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, 1);
 	  HAL_Delay(1000);
-	  HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, 0);
+	  HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, 1);
 	  HAL_Delay(1000);
-	  HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, 0);
+	  HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, 1);
 	  HAL_Delay(1000);
-	  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, 0);
+	  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, 1);
 	  HAL_Delay(1000);
-	  HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, 0);
+	  HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, 1);
 
 
     /* USER CODE END WHILE */
