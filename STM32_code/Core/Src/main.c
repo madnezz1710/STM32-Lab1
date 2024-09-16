99200c9d178a5ce177c00708bc4fce084269ae09
LITTLEMADNEZZ\itzic <itzicehandsome@gmail.com>
2024-09-16 21:54:15 +0700

    update for ex3

--- STM32_code/Core/Src/main.c
+++ STM32_code/Core/Src/main.c
@@ -91,60 +91,92 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  int timecount=0;
+  int part=0;
   while (1)
   {
-	  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin,1);
-	  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin,0);
-	  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin,0);
-	  HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin,0);
-	  HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin,0);
-	  HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin,1);
-	  HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin,1);
-	  HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin,0);
-	  HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin,0);
-	  HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin,0);
-	  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin,0);
-	  HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin,1);
-	  HAL_Delay(3000);
-	  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin,1);
-	  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin,0);
-	  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin,0);
-	  HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin,0);
-	  HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin,1);
-	  HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin,0);
-	  HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin,1);
-	  HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin,0);
-	  HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin,0);
-	  HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin,0);
-	  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin,1);
-	  HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin,0);
-	  HAL_Delay(2000);
-	  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin,0);
-	  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin,0);
-	  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin,1);
-	  HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin,1);
-	  HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin,0);
-	  HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin,0);
-	  HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin,0);
-	  HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin,0);
-	  HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin,1);
-	  HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin,1);
-	  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin,0);
-	  HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin,0);
-	  HAL_Delay(3000);
-	  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin,0);
-	  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin,1);
-	  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin,0);
-	  HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin,1);
-	  HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin,0);
-	  HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin,0);
-	  HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin,0);
-	  HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin,1);
-	  HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin,0);
-	  HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin,1);
-	  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin,0);
-	  HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin,0);
-	  HAL_Delay(2000);
+	  switch (part) {
+		case 0:
+			  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin,1);
+			  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin,0);
+			  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin,0);
+			  HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin,0);
+			  HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin,0);
+			  HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin,1);
+			  HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin,1);
+			  HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin,0);
+			  HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin,0);
+			  HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin,0);
+			  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin,0);
+			  HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin,1);
+			  timecount++;
+			  if(timecount==3){
+				  timecount=0;
+				  part=1;
+			  }
+			break;
+		case 1:
+			  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin,1);
+			  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin,0);
+			  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin,0);
+			  HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin,0);
+			  HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin,1);
+			  HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin,0);
+			  HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin,1);
+			  HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin,0);
+			  HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin,0);
+			  HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin,0);
+			  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin,1);
+			  HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin,0);
+			  timecount++;
+			  if(timecount==2){
+				  timecount=0;
+				  part=2;
+			  }
+			break;
+		case 2:
+			  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin,0);
+			  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin,0);
+			  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin,1);
+			  HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin,1);
+			  HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin,0);
+			  HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin,0);
+			  HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin,0);
+			  HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin,0);
+			  HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin,1);
+			  HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin,1);
+			  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin,0);
+			  HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin,0);
+			  timecount++;
+			  if(timecount==3){
+				  timecount=0;
+				  part=3;
+			  }
+			break;
+		case 3:
+			  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin,0);
+			  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin,1);
+			  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin,0);
+			  HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin,1);
+			  HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin,0);
+			  HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin,0);
+			  HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin,0);
+			  HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin,1);
+			  HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin,0);
+			  HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin,1);
+			  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin,0);
+			  HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin,0);
+			  timecount++;
+			  if(timecount==2){
+				  timecount=0;
+				  part=0;
+			  }
+			break;
+		default:
+			break;
+	}
+
+	  HAL_Delay(1000);
 
     /* USER CODE END WHILE */
 
